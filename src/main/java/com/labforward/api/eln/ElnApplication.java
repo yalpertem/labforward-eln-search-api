db6fc69bd7163881a4d5af01ac51d23345113f68
Yigitalp Ertem <yalpertem@gmail.com>
2020-08-08 21:14:27 +0300

    Inital commit (generated via spring initializr).

---
+++ src/main/java/com/labforward/api/eln/ElnApplication.java
@@ -0,0 +1,13 @@
+package com.labforward.api.eln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labforward/api/eln/ElnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labforward.api.eln;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
